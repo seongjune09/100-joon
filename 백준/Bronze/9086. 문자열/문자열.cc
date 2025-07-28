8c131cd0a47467abe6bbb9ae8a5c0004f98e6668
Seongjun09 <freestarjun@gmail.com>
2025-07-28 12:54:11 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.cc
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -1,40 +1,18 @@
 #include <stdio.h>
-
 int main(void) {
     int n;
     scanf("%d", &n);
-    getchar();  // 숫자 입력 뒤 남은 개행 문자 제거
 
-    char results[100][3];  // 최대 100줄, 각 줄 최대 2글자 + '\0'
+    char str[101];
 
-    for (int i = 0; i < n; i++) {
-        char a;
-        char str[1001];
-        int idx = 0;
+    for(int i=0; i<n; i++) {
+        scanf("%s", str);
 
-        while (1) {
-            a = getchar();
-            if (a == '\n' || a == EOF) break;
-            if (a != ' ') {
-                str[idx++] = a;
-            }
-        }
-
-        if (idx == 1) {
-            // 한 글자면 같은 문자 두 번 저장
-            results[i][0] = str[0];
-            results[i][1] = str[0];
-            results[i][2] = '\0';
-        } else {
-            results[i][0] = str[0];
-            results[i][1] = str[idx - 1];
-            results[i][2] = '\0';
-        }
+        int len = 0;
+        while (str[len] != '\0') {
+            len ++;
+        } 
+        printf("%c%c\n", str[0], str[len-1]);
     }
-
-    for (int i = 0; i < n; i++) {
-        printf("%s\n", results[i]);
-    }
-
     return 0;
 }
