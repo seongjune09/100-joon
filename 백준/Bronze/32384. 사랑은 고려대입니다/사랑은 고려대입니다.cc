66faa79c31bc608a354ecd8c3f0f4f9cbf7c88ed
Seongjun09 <freestarjun@gmail.com>
2025-09-25 23:33:00 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) {
+    
+    int n;
+    scanf("%d", &n);
+    
+    for(int i=0; i<n; i++) {
+            printf("LoveisKoreaUniversity ");
+        }
+    return 0;
+}
