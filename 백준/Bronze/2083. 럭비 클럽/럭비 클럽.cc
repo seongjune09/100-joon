d5545bb141e7a8ae7b684df59554fdd66a4e2784
박성준 <freestarjun@gmail.com>
2026-03-17 23:46:03 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.cc
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char name[50];
+    int age, weight;
+
+    while(1) {
+        scanf("%s %d %d", name, &age, &weight);
+
+        if(name[0] == '#' && age == 0 && weight == 0) {
+            break;
+        }
+
+        if(age > 17 || weight >= 80) {
+            printf("%s Senior\n", name);
+        } else {
+            printf("%s Junior\n", name);
+        }
+    }
+
+    return 0;
+}
